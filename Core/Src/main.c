1fc356fa32eeba1af48c5346daeb30fb687955bf
jmac1 <145376972+JMacPort@users.noreply.github.com>
2024-12-13 12:33:16 -0500

    OLED display turns on and displays. Functionality to write to the screen is needed along with the proper intitialization commands

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,7 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+// Prototypes
+void USART_Init();
+void USART_Print(const char* str, ...);
+void I2C_Init();
+void OLED_Init();
+//void find_addr();
+
+// OLED
+#define OLED_ADDR					0x3C
+#define OLED_CMD					0x00
 
 int main() {
+	USART_Init();
+	I2C_Init();
+	OLED_Init();
 
+	for (volatile int i = 0; i < 60000; i++);
+
+	while (1) {
+
+	}
 }
 
 void USART_Init() {
@@ -14,8 +35,23 @@ void USART_Init() {
 	GPIOA -> AFR[0] |= (7 << (4 * 2)) | (7 << (4 * 3));					// AFR set to USART2
 
 	USART2 -> BRR = 0x0683;												// Baud Rate 9600
-	USART2 -> CR1 |= (1 << 3) | (1 << 13);											// Transmit Enable; USART2 Enable
+	USART2 -> CR1 |= (1 << 3) | (1 << 13);								// Transmit Enable; USART2 Enable
+}
 
+void USART_Print(const char* str, ...) {
+    char buffer[100];
+
+    va_list args;
+
+    va_start(args, str);
+    vsprintf(buffer, str, args);
+    va_end(args);
+
+    char* ptr = buffer;
+    while(*ptr) {
+        while(!(USART2->SR & (1 << 7)));
+        USART2->DR = *ptr++;
+    }
 }
 
 void I2C_Init() {
@@ -61,7 +97,11 @@ void I2C_Start() {
 void I2C_SendAddress(uint8_t address, uint8_t read) {
 	I2C1 -> DR = (address << 1) | read;
 
-	while (!(I2C1 -> SR1 & (1 << 1)));
+	uint32_t timeout = 10000;
+	while(!(I2C1->SR1 & (1 << 1)) && timeout) timeout--;  // Wait for ADDR with timeout
+	if(timeout == 0) {
+	   USART_Print("Address timeout\r\n");
+	}
 
 	uint8_t dummyRead = I2C1 -> SR1;
 	dummyRead = I2C1 -> SR2;
@@ -88,11 +128,45 @@ void I2C_Write(uint8_t address, uint8_t data) {
 	I2C_Stop();
 }
 
-void find_addr() {
-
-
-
+void OLED_Send_Command(uint8_t cmd) {
+    I2C_Start();
+    I2C_SendAddress(OLED_ADDR, 0);
+    I2C_SendData(OLED_CMD);
+    I2C_SendData(cmd);
+    I2C_Stop();
 }
+
+void OLED_Init() {
+    // Turn display off first
+    OLED_Send_Command(0xAE);    // Display off
+
+    // Power settings
+    OLED_Send_Command(0x8D);    // Charge pump setting
+    OLED_Send_Command(0x14);    // Enable charge pump
+
+    // Turn display on
+    OLED_Send_Command(0xAF);    // Display on
+}
+
+//void find_addr() {
+//    USART_Print("Starting I2C scan...\r\n");
+//
+//    for (int i = 0; i < 128; i++) {
+//        I2C_Start();
+//        USART_Print("Trying address: 0x%X\r\n", i);
+//
+//        I2C_SendAddress(i, 0);
+//
+//        if (!(I2C1->SR1 & (1 << 10))) {
+//            USART_Print("Found device at: 0x%X\r\n", i);
+//        }
+//
+//        I2C1->SR1 &= ~(1 << 10);
+//        I2C_Stop();
+//    }
+//
+//    USART_Print("Scan complete\r\n");
+//}
 
 
 
